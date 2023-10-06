d0f6d14e1feecdfee7a938def228cd4801f37109
ysabelvalencia <ysavalencia@hotmail.com>
2023-10-06 10:31:36 +0200

    install react kit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+//imports dependencias, imagenes, componentes, estilos
+
+
+// import '../styles/App.css';
+// import nombreVariable from '../images/nombre-imagen';
+
+
+
+function App() {
+
+  //funciones, variables, handles
+
+  return (
+  //html
+    /* <> </> etiqueta vacia = fragments */
+  <>
+
+  </>
+    
+  );
+}
+
+export default App;
